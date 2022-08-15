ebaeb55945f1f8e92f5ba4d4a7a375b019cf2b49
youssef.abdelrhafour <youssef.abdelrhafour@IC-CS233-40.uc.ucalgary.ca>
2022-08-14 20:03:59 -0600

    Created the password manager login screen, including UI appearance.

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,7 +5,7 @@ import java.io.FileInputStream;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 
@@ -14,13 +14,13 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader();
-			VBox root = loader.load(new FileInputStream("src/application/Scene1.fxml"));
+			AnchorPane root = loader.load(new FileInputStream("src/application/LoginScreen.fxml"));
 			PasswordManagerController controller =(PasswordManagerController) loader.getController();
 
 			controller.applicationStage = primaryStage;		
 
 
-			Scene scene = new Scene(root,500,500);
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
