2b91d104ada57533697e7a675dcb661a8c6c4708
youssef.abdelrhafour <youssef.abdelrhafour@IC-CS233-26.uc.ucalgary.ca>
2022-08-16 13:29:00 -0600

    Last edits made to UI. Password Manager (PassVault) should now be done.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,35 +1,28 @@
 package application;
-	
-import java.io.FileInputStream;
-import java.io.IOException;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
 	@Override
 	public void start(Stage stage) {
 		try {
-		
+
 			AnchorPane root = FXMLLoader.load(getClass().getResource("LoginScreen.fxml"));
 			Scene scene = new Scene(root);
 			stage.setScene(scene);
 			stage.show();
-			
-		} catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
-	
+
 }
